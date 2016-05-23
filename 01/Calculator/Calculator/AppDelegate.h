0e88277f3952168fdc5ff32fd462143fb0ed217e
josmoss <josmoss@gmail.com>
2016-05-23 13:38:54 -0600

    Adding Calculator Assignment

---
+++ 01/Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Joe Moss on 5/23/16.
+//  Copyright © 2016 Iron Yard_Joe Moss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01/Calculator/Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Joe Moss on 5/23/16.
+//  Copyright © 2016 Iron Yard_Joe Moss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
